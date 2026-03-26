9da4e3f19da106f9feec64cb3f39a4bcbb0f3e49
Insightlibrary <insightlibrary451@gmail.com>
2026-03-26 18:05:28 +0100

    Add endpoint to fetch posts from database

--- index.js
+++ index.js
@@ -143,6 +143,16 @@ app.get("/search", async (req, res) => {
 });
 
 
+app.get("/test-db", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 
 
 /* ---------------- LOGGER MIDDLEWARE ---------------- */
